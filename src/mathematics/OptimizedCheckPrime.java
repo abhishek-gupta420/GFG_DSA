89ce4e7797fba93a2eb38d02f78a1a3714a3a0f5
abhishek-gupta420 <abhishek.guru420@gmail.com>
2025-04-25 23:26:48 +0530

    prime number code optimized

---
+++ src/mathematics/OptimizedCheckPrime.java
@@ -0,0 +1,22 @@
+package mathematics;
+
+public class OptimizedCheckPrime {
+    public static void main(String[] args) {
+        System.out.println(isPrime(2));
+        System.out.println(isPrime(3));
+        System.out.println(isPrime(4));
+        System.out.println(isPrime(523));
+        System.out.println(isPrime(100));
+    }
+
+    public static boolean isPrime(int n) {
+        if (n == 1) return false;
+        if (n == 2  || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+        }
+
+        return true;
+    }
+}

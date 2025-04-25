5acc3bb9e731b6632e977f6c8c4d8eb7601b4dfd
abhishek-gupta420 <abhishek.guru420@gmail.com>
2025-04-25 23:07:11 +0530

    prime number code simple logic added

---
+++ src/mathematics/CheckPrime.java
@@ -0,0 +1,19 @@
+package mathematics;
+
+public class CheckPrime {
+    public static void main(String[] args) {
+        System.out.println(isPrime(2));
+        System.out.println(isPrime(3));
+        System.out.println(isPrime(4));
+        System.out.println(isPrime(523));
+        System.out.println(isPrime(100));
+    }
+
+    static boolean isPrime(int n) {
+        if (n == 1) return false;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
